e9923311e23f0393900e5ca8260358ddeddad2ee
Sarthak Krishak <sarthakkrishak1234@gmail.com>
2025-06-17 13:05:43 +0530

    imagekit video api

---
+++ app/components/Provider.tsx
@@ -0,0 +1,18 @@
+import { ImageKitProvider } from "@imagekit/next";
+import { SessionProvider } from "next-auth/react"
+
+const urlEndPoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+
+if (!urlEndPoint) {
+    console.log("No url endpoint recieved");
+}
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+    return (
+      <SessionProvider refetchInterval={5 * 60}>
+        <ImageKitProvider urlEndpoint={urlEndPoint}>
+          {children}
+        </ImageKitProvider>
+      </SessionProvider>
+    );
+}
